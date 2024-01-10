76ad0cafe947afc08fb2b7853d8e95b1fbd3a087
fitriarhmdni15 <128965425+fitriarhmdni15@users.noreply.github.com>
2024-01-10 18:12:47 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Media Online</h1>
+        <h2>Sosial Media Developer</h2>
+        <p>Belajar dan berbagi agar hidup menjadi lebih baik</p>
+        <h3>Benefit join di Media Online</h3>
+        <ul>
+            <li>Mendapatkan motivasi dari sesama para Developer</li>
+            <li>Sharing knowlenge</li>
+            <li>Dibuat oleh calon web developer terbaik</li>
+        </ul>
+        <h3>Cara Bergabung ke Media Online</h3>
+        <ol>
+            <li>Mengunjungi website ini</li>
+            <li>Mendaftarkan di <a href="form.php">Form Sign Up</a></li>
+            <li>Selesai</li>
+        </ol>
+    </body>
+</html>
